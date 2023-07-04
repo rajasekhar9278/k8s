f8287568abcb480fb88005f90f6330603b3fde05
rajasekhar9278 <rajasrav61@gmail.com>
2023-07-04 14:41:39 +0530

    added file changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.6.2"
+      version = "~>5.6"
     }
   }
 }
 
 provider "aws" {
-  features {
     
-  }
 }
 
 
