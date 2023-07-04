a62393b557c50e99f7fd7284a18d09df97a74a30
rajasekhar9278 <rajasrav61@gmail.com>
2023-07-04 13:36:36 +0530

    added file

---
+++ terraform/variable.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "regions my resources will be deployed"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-04581fbf744a7d11f"
+}
+
+variable "key_name" {
+  type    = string
+  default = "myec2key"
+}
+
+variable "s3bucket" {
+  type    = string
+  default = "jenkins-bucket-31032023369432"
+}
+
+variable "acl" {
+  type    = string
+  default = "private"
+}
+
+variable "vpc_id" {
+  type    = string
+  default = "vpc-084f2b2f58db22b55"
+}
